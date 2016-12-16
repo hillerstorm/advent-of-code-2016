2650d14c0c2135611c338c39b4784549dd065d63
Johan Hillerström <progr@mmer.nu>
2016-12-16 17:26:49 +0100

    Add Day 4

---
+++ Day4/Part1.elm
@@ -0,0 +1,75 @@
+module Day4.Part1 exposing (main, validRoom)
+
+import Html exposing (..)
+import Char exposing (isLower)
+import Day4.Input exposing (rawInput, parsedInput, Room)
+
+
+unique : List Char -> List Char -> List Char
+unique result list =
+    case list of
+        [] ->
+            result
+
+        x :: xs ->
+            if List.member x result then
+                unique result xs
+            else
+                unique (x :: result) xs
+
+
+count : Char -> String -> Int
+count chr =
+    String.length << String.filter ((==) chr)
+
+
+compareChars : String -> Char -> Char -> Order
+compareChars name a b =
+    let
+        aCount =
+            count a name
+
+        bCount =
+            count b name
+
+        diff =
+            compare bCount aCount
+    in
+        case diff of
+            EQ ->
+                compare a b
+
+            _ ->
+                diff
+
+
+validRoom : Room -> Bool
+validRoom { name, sector, checksum } =
+    let
+        calcChecksum =
+            name
+                |> String.filter isLower
+                |> String.toList
+                |> unique []
+                |> List.sort
+                |> List.sortWith (compareChars name)
+                |> List.take 5
+                |> String.fromList
+    in
+        checksum == calcChecksum
+
+
+filterValid : Room -> Maybe Int
+filterValid room =
+    if validRoom room then
+        Just room.sector
+    else
+        Nothing
+
+
+main : Html msg
+main =
+    div []
+        [ div [] [ text ("Input: " ++ rawInput) ]
+        , div [] [ text ("Result: " ++ (toString <| List.sum <| List.filterMap filterValid parsedInput)) ]
+        ]
---
+++ Day4/Part2.elm
@@ -0,0 +1,52 @@
+module Day4.Part2 exposing (main)
+
+import Html exposing (..)
+import Char exposing (fromCode, toCode)
+import Day4.Part1 exposing (validRoom)
+import Day4.Input exposing (rawInput, parsedInput, Room)
+
+
+shift : Int -> Char -> Char
+shift steps chr =
+    case steps of
+        0 ->
+            chr
+
+        _ ->
+            case chr of
+                '-' ->
+                    ' '
+
+                'z' ->
+                    shift (steps - 1) 'a'
+
+                _ ->
+                    shift (steps - 1) <| fromCode <| (+) 1 <| toCode chr
+
+
+decrypt : Room -> Room
+decrypt room =
+    { room | name = String.map (shift room.sector) room.name }
+
+
+isStorage : Room -> Bool
+isStorage room =
+    room.name == "northpole object storage"
+
+
+solve : List Room -> Maybe Int
+solve rooms =
+    rooms
+        |> List.filter validRoom
+        |> List.map decrypt
+        |> List.filter isStorage
+        |> List.head
+        |> Maybe.andThen (\r -> Just r.sector)
+
+
+main : Html msg
+main =
+    div []
+        [ div [] [ text ("Input: " ++ rawInput) ]
+        , div [] [ text ("Result: " ++ (toString <| solve parsedInput)) ]
+        ]
